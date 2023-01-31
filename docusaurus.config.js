cd7754d0cce7ad1051443b8b5e206a9facf9d2c2
Flas4hPT <93576144+TiagoVXII@users.noreply.github.com>
2023-01-31 21:21:32 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   
 
   // Set the production url of your site here
-  url: 'https://novowebsiteministro.pages.dev/',
+  url: 'https://ministrodajustica.pages.dev/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
